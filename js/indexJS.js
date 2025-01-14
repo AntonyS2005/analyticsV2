c96c60cb0720dc189be37ade967f08d88869de3f
antonySantos <santoslopeztony@gmail.com>
2025-01-13 21:15:57 -0600

    trabajando en los menus

---
+++ js/indexJS.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("btnStar");
+
+button.addEventListener("click", function () {
+  window.location.href = "menu.html"; // Ruta correcta del archivo
+});
---
+++ js/menu.js
@@ -0,0 +1,4 @@
+const btnAnalisis = document.getElementById("btnAnalisis");
+btnAnalisis.addEventListener("click",function(){
+    window.location.href = "subMenuAnalisis.html"
+});
